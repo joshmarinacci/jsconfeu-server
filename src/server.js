8bed8590244b83f37292977be7c7a6de3017e70c
josh <joshua@marinacci.org>
2018-10-16 16:17:27 -0700

    added more users to the github list

--- src/server.js
+++ src/server.js
@@ -17,7 +17,8 @@ const SECRETS = {
 }
 console.log("using the secrets",SECRETS)
 
-const ADMIN_USERS = ['joshmarinacci','slightlyoffbeat','sandrapersing','linclark','tschneidereit','potch']
+const ADMIN_USERS = ['joshmarinacci','slightlyoffbeat','sandrapersing','linclark','tschneidereit'
+    ,'potch','SteveKlabnik','Ashleygwilliams','Mykmelez']
 
 
 const DB_FILE = path.join(process.cwd(),'modules.db')
